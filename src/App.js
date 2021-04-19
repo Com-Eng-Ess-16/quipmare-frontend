a6dd5ba27317da5b0a6730737939e9aaa4424f28
PKhing <apimrak@gmail.com>
2021-04-19 14:21:00 +0700

    Add jsconfig to avoid long import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from 'logo.svg'
+import 'App.css'
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core'
-import theme from './config/theme'
+import theme from 'config/theme'
 
 function App() {
   return (
