050d33467d6758dc123750eb2ded6812346fac1e
PKhing <apimrak@gmail.com>
2021-04-20 22:14:31 +0700

    Try to fix CI/CD

--- src/initFirebase.js
+++ src/initFirebase.js
@@ -5,6 +5,7 @@ function initFirebase() {
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    projectId: 'quipmare-game',
   }
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
