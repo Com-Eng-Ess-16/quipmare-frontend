3ae2afe010bc44f7c6c9db5678e9e4b37f506c3e
PKhing <apimrak@gmail.com>
2021-04-20 22:01:55 +0700

    Connect with firebase database

---
+++ src/initFirebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+function initFirebase() {
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  }
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+    var testRef = firebase.database().ref('test')
+    testRef.off()
+    testRef.on('value', (snapshot) => {
+      const data = snapshot.val()
+      console.log('here')
+      console.log(data)
+    })
+  }
+}
+export default initFirebase
