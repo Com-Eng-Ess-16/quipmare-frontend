a230abcb9939e109820470bf2a1512d6a9b26197
PKhing <apimrak@gmail.com>
2021-04-21 20:35:02 +0700

    Display member list in waiting room

--- src/components/pages/waitingRoom/index.js
+++ src/components/pages/waitingRoom/index.js
@@ -1,4 +1,28 @@
+import { useContext } from 'react'
+import { MemberContext, UserContext } from 'context/context'
+import { postStartGame } from 'utils/apiService'
+
+const { Typography, Button } = require('@material-ui/core')
+
 function WaitingRoom() {
-  return <>This is Waiting Room</>
+  const userContext = useContext(UserContext)
+  const memberContext = useContext(MemberContext)
+
+  return (
+    <>
+      <Typography>{userContext.roomCode}</Typography>
+
+      <Typography>memberList</Typography>
+      {memberContext.member.map((val, key) => {
+        return <Typography>{val}</Typography>
+      })}
+
+      {userContext.userID === 0 && (
+        <Button variant="contained" color="primary" onClick={postStartGame}>
+          start
+        </Button>
+      )}
+    </>
+  )
 }
 export default WaitingRoom
