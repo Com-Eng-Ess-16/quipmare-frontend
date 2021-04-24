471e263a962e3a6925bc17efab526fde67c9b6c8
PKhing <apimrak@gmail.com>
2021-04-25 02:11:48 +0700

    Implement Countdown component

--- src/components/common/Countdown.js
+++ src/components/common/Countdown.js
@@ -1,10 +1,43 @@
-import { Typography } from '@material-ui/core'
+import { makeStyles, Typography } from '@material-ui/core'
 import { useContext } from 'react'
 import { default as ReactCountdown } from 'react-countdown'
 import { postCountdownEnd } from 'utils/apiService'
 import { UserContext } from 'context/context'
+import { getColor } from 'utils/colorUtil'
+const useStyles = makeStyles((theme) => ({
+  container: {
+    right: '20px',
+    bottom: '20px',
+    position: 'absolute',
+  },
+  text: {
+    fontSize: '3rem',
+    fontFamily: 'Architects Daughter',
+    display: 'inline-block',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2rem',
+    },
+  },
+  time: {
+    fontSize: '3rem',
+    background: (props) => props.dark,
+    fontFamily: 'Architects Daughter',
+    display: 'inline-block',
+    minWidth: '70px',
+    textAlign: 'center',
+    borderRadius: '100px',
+    marginLeft: '30px',
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: '20px',
+      fontSize: '2rem',
+      minWidth: '45px',
+    },
+  },
+}))
 export default function Countdown(props) {
   const userContext = useContext(UserContext)
+  const color = '0'
+  const styles = useStyles(getColor(color))
   return (
     <ReactCountdown
       date={userContext.gameData.countdownEnd}
@@ -14,9 +47,10 @@ export default function Countdown(props) {
         }
       }}
       renderer={(prop) => (
-        <Typography>
-          {`timeLeft : ${prop.seconds + prop.minutes * 60} seconds`}
-        </Typography>
+        <div className={styles.container}>
+          <Typography className={styles.text}>Ending in...</Typography>
+          <div className={styles.time}>{prop.seconds + prop.minutes * 60}</div>
+        </div>
       )}
     />
   )
