c5e3709dc683f430298d0a60cea4800e0dbd3f24
luangjay <zeusstyle69@gmail.com>
2021-04-27 00:45:15 +0700

    Edit home page, rome code input page

--- src/components/pages/home/roomCodeInput/index.js
+++ src/components/pages/home/roomCodeInput/index.js
@@ -6,30 +6,44 @@ import { PlayerContext, UserContext } from 'context/context'
 import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
-  joinText: {
-    fontFamily: 'Architects Daughter',
-    marginTop: '12.5vh'
+  page: {
+    margin: '10vh 5vw 0 5vw'
   },
-  buttons: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: '30vw'
+  actionText: {
+    fontFamily: 'Architects Daughter',
+    fontSize: '3.5rem',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.9rem'
+    }
   },
   roomCodeInput: {
-    marginTop: '15vh',
-    width: '400px',
-    marginBottom: '15vh'
+    margin: '10vh 0 20vh 0',
+    width: '70%',
+    fontSize: '2rem',
+    [theme.breakpoints.down('sm')]: {
+      margin: '10vh 0 15vh 0'
+    }
+  },
+  buttons: {
+    marginLeft: '60%',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      marginLeft: '50%'
+    },
   },
   button: {
-    fontSize: '2.5rem',
+    fontSize: '1.5rem',
     fontFamily: 'Architects Daughter',
-    width: '250px',
-    height: '100px',
+    width: '15vw',
+    marginLeft: '13%',
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
     border: '2px solid black',
-    marginRight: '40px'
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      fontSize: '1rem',
+      margin: '20% 0 0% 0%'
+    },
   }
 }))
 
@@ -46,8 +60,8 @@ function RoomCodeInput(action) {
     }
   }
   return (
-    <>
-      <Typography className={styles.joinText} variant='h3'>{action.action.toUpperCase()+' A ROOM'}</Typography>
+    <div className={styles.page}>
+      <Typography className={styles.actionText}>{action.action.toUpperCase()+' A ROOM'}</Typography>
       <div>
         <TextField className={styles.roomCodeInput}
           label="room code"
@@ -58,10 +72,10 @@ function RoomCodeInput(action) {
         />
         </div>
       <div className={styles.buttons}>
-        <Button className={styles.button} onClick={joinRoom} variant="contained">JOIN</Button>
-        <Button className={styles.button} variant="contained">LEAVE</Button>
+          <Button className={styles.button} onClick={joinRoom} variant="contained">JOIN</Button>
+          <Button className={styles.button} variant="contained">LEAVE</Button>
       </div>
-    </>
+    </div>
   )
 }
 export default RoomCodeInput
