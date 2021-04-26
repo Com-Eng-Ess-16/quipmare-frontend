2185b49b019ae08ce901259c8c42e87c22eb190c
PKhing <apimrak@gmail.com>
2021-04-27 02:06:51 +0700

    Implement podium page

--- src/components/pages/gameRoom/Podium/index.js
+++ src/components/pages/gameRoom/Podium/index.js
@@ -1,39 +1,79 @@
-import { Button, Typography } from '@material-ui/core'
-import { UserContext } from 'context/context'
-import { useContext, useEffect, useState } from 'react'
-import { getStanding, postBackToWaiting } from 'utils/apiService'
+import { Box, Button, makeStyles, Typography } from '@material-ui/core'
+import PodiumItem from './PodiumItem'
+const useStyles = makeStyles((theme) => ({
+  text: {
+    marginTop: '-10px',
+    fontSize: '3.5rem',
+    fontFamily: 'Architects Daughter',
+    [theme.breakpoints.down('md')]: {
+      marginTop: 0,
+      fontSize: '3rem',
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2rem',
+    },
+
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '1.75rem',
+    },
+  },
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexGrow: 1,
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+    },
+  },
+  buttonContainer: {
+    display: 'flex',
+    marginTop: '20px',
+    alignItems: 'center',
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column-reverse',
+      alignItems: 'stretch',
+    },
+  },
+  button: {
+    fontSize: '2rem',
+    fontFamily: 'Architects Daughter',
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '10px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '1.75rem',
+    },
+  },
+}))
 function Podium() {
-  const userContext = useContext(UserContext)
-  const [score, setScore] = useState(null)
-  useEffect(() => {
-    async function getData() {
-      const res = await getStanding(userContext.roomCode)
-      setScore(res)
-    }
-    getData()
-  }, [userContext.roomCode])
-  if (score === null) return <></>
+  const styles = useStyles()
   return (
-    <>
-      {Object.keys(score).map((key) => {
-        return (
-          <Typography>
-            {key.toString() + ': ' + score[key].toString()}
-          </Typography>
-        )
-      })}
-      {userContext.userID === 0 && (
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            postBackToWaiting(userContext.roomCode)
-          }}
-        >
-          Back to waiting room
+    <Box display="flex" flexDirection="column" height="100%">
+      <Typography className={styles.text}>
+        And the most popular player are...
+      </Typography>
+
+      <PodiumItem
+        size={Math.min(
+          (80 * window.innerWidth) / 100,
+          (60 * window.innerHeight) / 100
+        )}
+        userID={0}
+        score={300}
+        style={{ margin: 'auto' }}
+      />
+      <div className={styles.buttonContainer}>
+        <Button variant="outlined" color="primary" className={styles.button}>
+          LEAVE
         </Button>
-      )}
-    </>
+        <Button variant="contained" color="primary" className={styles.button}>
+          play again
+        </Button>
+      </div>
+    </Box>
   )
 }
 export default Podium
