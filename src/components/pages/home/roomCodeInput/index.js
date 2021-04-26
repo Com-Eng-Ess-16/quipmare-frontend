886b324947a034e7f7bb8bbec217b718e161e8a7
luangjay <zeusstyle69@gmail.com>
2021-04-26 13:08:47 +0700

    Implement home page, profile page, room code input page

--- src/components/pages/home/roomCodeInput/index.js
+++ src/components/pages/home/roomCodeInput/index.js
@@ -1,9 +1,40 @@
-import { Button, TextField } from '@material-ui/core'
+import { Button, TextField, Typography } from '@material-ui/core'
 import { useContext, useState } from 'react'
 import { getIsRoomExist } from 'utils/apiService'
 import { addPlayerListener } from 'utils/firebaseUtil'
 import { PlayerContext, UserContext } from 'context/context'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  joinText: {
+    fontFamily: 'Architects Daughter',
+    marginTop: '12.5vh'
+  },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: '30vw'
+  },
+  roomCodeInput: {
+    marginTop: '15vh',
+    width: '400px',
+    marginBottom: '15vh'
+  },
+  button: {
+    fontSize: '2.5rem',
+    fontFamily: 'Architects Daughter',
+    width: '250px',
+    height: '100px',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    border: '2px solid black',
+    marginRight: '40px'
+  }
+}))
+
 function RoomCodeInput(action) {
+  const styles = useStyles()
   const playerContext = useContext(PlayerContext)
   const userContext = useContext(UserContext)
   const [roomCode, setRoomCode] = useState('')
@@ -16,14 +47,20 @@ function RoomCodeInput(action) {
   }
   return (
     <>
-      <TextField
-        label="room code"
-        value={roomCode}
-        onChange={(event) => {
+      <Typography className={styles.joinText} variant='h3'>{action.action.toUpperCase()+' A ROOM'}</Typography>
+      <div>
+        <TextField className={styles.roomCodeInput}
+          label="room code"
+          value={roomCode}
+          onChange={(event) => {
           setRoomCode(event.target.value)
-        }}
-      />
-      <Button onClick={joinRoom}>join</Button>
+            }}
+        />
+        </div>
+      <div className={styles.buttons}>
+        <Button className={styles.button} onClick={joinRoom} variant="contained">JOIN</Button>
+        <Button className={styles.button} variant="contained">LEAVE</Button>
+      </div>
     </>
   )
 }
