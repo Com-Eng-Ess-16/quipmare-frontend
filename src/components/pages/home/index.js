3787ba299160e321b50445004755d63e31df08ec
luangjay <zeusstyle69@gmail.com>
2021-04-27 00:45:15 +0700

    Edit home page, rome code input page

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -9,90 +9,74 @@ import { makeStyles } from '@material-ui/core'
 //import zIndex from '@material-ui/core/styles/zIndex'
 
 const useStyles = makeStyles((theme) => ({
+  page: {
+    margin: '13vh 0 0 0',
+    [theme.breakpoints.down('sm')]: {
+      margin: '7vh 3vh 0 3vh'
+    },
+    [theme.breakpoints.down('xs')]: {
+      margin: '10vh 3vh 0 3vh'
+    }
+  },
   gameTitle: {
-    //fontSize: '3.5rem',
-    marginTop: '100px',
-    fontFamily: 'Architects Daughter'
+    fontSize: '3.5rem',
+    display: 'flex',
+    justifyContent: 'center',
+    fontFamily: 'Architects Daughter',
+    marginBottom: '15vh',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2.5rem'
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '2rem'
+    }
   },
   buttons: {
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: '25px'
+    justifyContent: 'center'
   },
   joinButton: {
-    marginTop: '80px',
     width: '100%',
-    height: '100px',
+    height: '20%',
     fontSize: '2.5rem',
     border: '3px solid',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '1.8rem'
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '1.6rem'
+    }
   },
   spectateButton: {
     width: '47.5%',
-    height: '100px',
-    marginRight: '5%',
+    height: '15%',
     fontSize: '2.5rem',
+    marginTop: '6vh',
     border: '3px solid',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '1.8rem'
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '1.6rem'
+    }
   },
   hostButton: {
     width: '47.5%',
-    height: '100px',
+    height: '15%',
     fontSize: '2.5rem',
+    marginTop: '6vh',
+    marginLeft: '5vw',
     border: '3px solid',
-    backgroundColor: 'white'
-  },
-  colorAvatar1: {
-    color: '#FFFFFF',
-    backgroundColor: '#FFFFFF',
-    border: '1px solid black',
-    width: '250px',
-    height: '250px',
-    marginTop: '-120px',
-    marginBottom: '-150px',
-    marginLeft: '-50px',
-    zIndex: '-1'
-  },
-  colorAvatar2: {
-    color: '#FFFFDC',
-    backgroundColor: '#FFFFDC',
-    border: '1px solid black',
-    width: '300px',
-    height: '300px',
-    marginTop: '150px',
-    marginBottom: '-430px',
-    marginLeft: '-450px',
-    zIndex: '-1'
-  },
-  colorAvatar3: {
-    color: '#9BFFFF',
-    backgroundColor: '#9BFFFF',
-    border: '1px solid black',
-    width: '300px',
-    height: '300px',
-    marginLeft: '250px',
-    marginTop: '200px',
-    marginBottom: '-500px',
-    zIndex: '-1'
-  },
-  colorAvatar4: {
-    color: 'yellow',
-    backgroundColor: 'yellow',
-    width: '300px',
-    height: '300px',
-    marginTop: '-150px',
-    marginBottom: '-150px',
-    zIndex: '-1'
-  },
-  colorAvatar5: {
-    color: 'yellow',
-    backgroundColor: 'yellow',
-    width: '300px',
-    height: '300px',
-    marginTop: '-150px',
-    marginBottom: '-150px',
-    zIndex: '-1'
+    backgroundColor: 'white',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '1.8rem'
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '1.6rem'
+    }
   }
 }))
 
@@ -110,17 +94,9 @@ function Home() {
 
   if (action === '')
     return (
-      <>
+      <div className={styles.page}>
         <div>
-          <Avatar className={styles.colorAvatar1} variant="circle"></Avatar>
-        </div>
-        <div>
-          <Avatar className={styles.colorAvatar2} variant="circle"></Avatar>
-        </div>
-        <div>
-          <Typography className={styles.gameTitle} variant='h4'>
-            The Greatest Jester
-          </Typography>
+          <Typography className={styles.gameTitle}>The Greatest Jester</Typography>
         </div>
         <div className={styles.buttons}>
           <Button className={styles.joinButton} color="primary" variant="outlined" onClick={() => setAction('join')}>join</Button>
@@ -129,7 +105,7 @@ function Home() {
           <Button className={styles.spectateButton} color="primary" variant="outlined" onClick={() => setAction('spectate')}>spectate</Button>
           <Button className={styles.hostButton} color="primary" variant="outlined" onClick={createRoom}>host</Button>
         </div>
-      </>
+      </div>
     )
   if (userContext.roomCode !== null) return <Profile action={action} />
   if (action === 'join' || action === 'spectate')
