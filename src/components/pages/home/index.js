adbc10f266be0994a408c6ce5ff0a724b86f3f42
luangjay <zeusstyle69@gmail.com>
2021-04-26 13:08:47 +0700

    Implement home page, profile page, room code input page

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,12 +1,103 @@
-import { Button } from '@material-ui/core'
+import { Avatar, Button, Typography } from '@material-ui/core'
 import React, { useContext, useState } from 'react'
 import { addPlayerListener } from 'utils/firebaseUtil'
 import { getCreateRoom } from 'utils/apiService'
 import Profile from './profile'
 import RoomCodeInput from './roomCodeInput'
 import { PlayerContext, UserContext } from 'context/context'
+import { makeStyles } from '@material-ui/core'
+//import zIndex from '@material-ui/core/styles/zIndex'
+
+const useStyles = makeStyles((theme) => ({
+  gameTitle: {
+    //fontSize: '3.5rem',
+    marginTop: '100px',
+    fontFamily: 'Architects Daughter'
+  },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '25px'
+  },
+  joinButton: {
+    marginTop: '80px',
+    width: '100%',
+    height: '100px',
+    fontSize: '2.5rem',
+    border: '3px solid',
+    backgroundColor: 'white'
+  },
+  spectateButton: {
+    width: '47.5%',
+    height: '100px',
+    marginRight: '5%',
+    fontSize: '2.5rem',
+    border: '3px solid',
+    backgroundColor: 'white'
+  },
+  hostButton: {
+    width: '47.5%',
+    height: '100px',
+    fontSize: '2.5rem',
+    border: '3px solid',
+    backgroundColor: 'white'
+  },
+  colorAvatar1: {
+    color: '#FFFFFF',
+    backgroundColor: '#FFFFFF',
+    border: '1px solid black',
+    width: '250px',
+    height: '250px',
+    marginTop: '-120px',
+    marginBottom: '-150px',
+    marginLeft: '-50px',
+    zIndex: '-1'
+  },
+  colorAvatar2: {
+    color: '#FFFFDC',
+    backgroundColor: '#FFFFDC',
+    border: '1px solid black',
+    width: '300px',
+    height: '300px',
+    marginTop: '150px',
+    marginBottom: '-430px',
+    marginLeft: '-450px',
+    zIndex: '-1'
+  },
+  colorAvatar3: {
+    color: '#9BFFFF',
+    backgroundColor: '#9BFFFF',
+    border: '1px solid black',
+    width: '300px',
+    height: '300px',
+    marginLeft: '250px',
+    marginTop: '200px',
+    marginBottom: '-500px',
+    zIndex: '-1'
+  },
+  colorAvatar4: {
+    color: 'yellow',
+    backgroundColor: 'yellow',
+    width: '300px',
+    height: '300px',
+    marginTop: '-150px',
+    marginBottom: '-150px',
+    zIndex: '-1'
+  },
+  colorAvatar5: {
+    color: 'yellow',
+    backgroundColor: 'yellow',
+    width: '300px',
+    height: '300px',
+    marginTop: '-150px',
+    marginBottom: '-150px',
+    zIndex: '-1'
+  }
+}))
 
 function Home() {
+  const styles = useStyles()
   const userContext = useContext(UserContext)
   const [action, setAction] = useState('')
   const playerContext = useContext(PlayerContext)
@@ -20,9 +111,24 @@ function Home() {
   if (action === '')
     return (
       <>
-        <Button onClick={() => setAction('join')}>join</Button>
-        <Button onClick={() => setAction('spectate')}>spectate</Button>
-        <Button onClick={createRoom}>host</Button>
+        <div>
+          <Avatar className={styles.colorAvatar1} variant="circle"></Avatar>
+        </div>
+        <div>
+          <Avatar className={styles.colorAvatar2} variant="circle"></Avatar>
+        </div>
+        <div>
+          <Typography className={styles.gameTitle} variant='h4'>
+            The Greatest Jester
+          </Typography>
+        </div>
+        <div className={styles.buttons}>
+          <Button className={styles.joinButton} color="primary" variant="outlined" onClick={() => setAction('join')}>join</Button>
+        </div>
+        <div className={styles.buttons}>
+          <Button className={styles.spectateButton} color="primary" variant="outlined" onClick={() => setAction('spectate')}>spectate</Button>
+          <Button className={styles.hostButton} color="primary" variant="outlined" onClick={createRoom}>host</Button>
+        </div>
       </>
     )
   if (userContext.roomCode !== null) return <Profile action={action} />
--- src/components/pages/home/profile/index.js
+++ src/components/pages/home/profile/index.js
@@ -1,11 +1,53 @@
-import { Button, TextField } from '@material-ui/core'
+import { Button, TextField, Card, CardContent, Typography, Avatar } from '@material-ui/core'
 import { useContext, useState } from 'react'
 import { postJoinRoom } from 'utils/apiService'
 import { addListener } from 'utils/firebaseUtil'
 import ColorInput from './ColorInput'
 import { UserContext } from 'context/context'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  cards: {
+    display: 'flex'
+  },
+  inputCard: {
+    marginTop: '5vh',
+    marginLeft: '-13vw',
+    width: '750px'
+  },
+  inputCardContent: {
+    marginLeft: '2vw'
+  },
+  usernameField: {
+    width: '300px',
+    marginTop: '20px'
+  },
+  selectText: {
+    fontFamily: 'Architects Daughter',
+    fontSize: '2.25rem',
+    marginTop: '3vh'
+  },
+  profileCard: {
+    marginTop: '5vh',
+    marginLeft: '3vw',
+    width: '400px'
+  },
+  profileCardContext: {
+    marginLeft: '2vw',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  profileAvatar: {
+    color: 'white',
+    backgroundColor: 'white',
+    border: '1px solid black',
+    width: '250px',
+    height: '250px'
+  }
+}))
 
 function Profile(action) {
+  const styles = useStyles()
   const [username, setUsername] = useState('')
   const [color, setColor] = useState('0')
   const userContext = useContext(UserContext)
@@ -21,15 +63,36 @@ function Profile(action) {
   }
   return (
     <>
-      <TextField
-        label="username"
-        value={username}
-        onChange={(event) => {
-          setUsername(event.target.value)
-        }}
-      />
-      <ColorInput color={color} setColor={setColor} />
-      <Button onClick={joinRoom}>Confirm</Button>
+      <div className={styles.cards}>
+        <Card className={styles.inputCard}>
+          <CardContent className={styles.inputCardContent}>
+            <Typography variant='h6'>
+              Username
+            </Typography>
+            <TextField className={styles.usernameField}
+              value={username}
+              onChange={(event) => {
+                setUsername(event.target.value)
+              }}
+            />
+            <Typography className={styles.selectText}>
+              Select your color
+            </Typography>
+            <ColorInput color={color} setColor={setColor} />
+          </CardContent>
+        </Card>
+        <Card className={styles.profileCard}>
+          <CardContent className={styles.profileCardContext}>
+              <Typography variant='h6'>
+                Profile
+              </Typography>
+              <Avatar className={styles.profileAvatar}>
+
+              </Avatar>
+          </CardContent>
+        </Card>
+        <Button onClick={joinRoom}>Confirm</Button>
+      </div>
     </>
   )
 }
