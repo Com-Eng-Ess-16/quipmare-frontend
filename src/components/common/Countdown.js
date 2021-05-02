ea6c6ed96ae6a17f5bc7ca03af8e96d2292e5f9b
PKhing <apimrak@gmail.com>
2021-05-03 03:27:31 +0700

    Send request if coutdown time less than 0

--- src/components/common/Countdown.js
+++ src/components/common/Countdown.js
@@ -1,4 +1,5 @@
 import { makeStyles, Typography } from '@material-ui/core'
+import { useEffect } from 'react'
 import { default as ReactCountdown } from 'react-countdown'
 import { postCountdownEnd } from 'utils/apiService'
 import { useAppController } from 'utils/appController'
@@ -46,21 +47,33 @@ export default function Countdown(props) {
   const appController = useAppController()
   const styles = useStyles(appController.getColor())
   const setError = useError()
+  const complete = async () => {
+    if (String(appController.userID) === String(0)) {
+      try {
+        await sleep(500)
+        await postCountdownEnd(appController.gameID)
+      } catch (err) {
+        setError(err)
+      }
+      appController.clearGameData()
+    }
+  }
+  // console.log(appController.countdownEnd - Date.now())
+  useEffect(() => {
+    if (!appController.countdownEnd) return
+    if (appController.countdownEnd - Date.now() < -4000) {
+      console.log('in')
+      console.log(appController.countdownEnd - Date.now())
+      complete()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [appController.countdownEnd])
+
   if (!appController.countdownEnd) return <></>
   return (
     <ReactCountdown
       date={appController.countdownEnd}
-      onComplete={async () => {
-        if (String(appController.userID) === String(0)) {
-          try {
-            await sleep(500)
-            await postCountdownEnd(appController.gameID)
-          } catch (err) {
-            setError(err)
-          }
-        }
-        appController.clearGameData()
-      }}
+      onComplete={complete}
       renderer={(prop) => (
         <div className={styles.container}>
           <Typography className={styles.text}>
