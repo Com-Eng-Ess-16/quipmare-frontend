c5eda791e3f80123ddd16ee5b1811ae1ca0d49b0
PKhing <apimrak@gmail.com>
2021-05-02 16:27:28 +0700

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core'
-import Header from 'components/shell/Header'
 import Pages from 'components/pages'
 import { initFirebase } from 'utils/firebaseUtil'
 import Div100vh from 'react-div-100vh'
@@ -18,18 +17,6 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     position: 'relative',
   },
-  body: {
-    height: '100%',
-    maxWidth: '1000px',
-    width: '90%',
-    margin: 'auto',
-    display: 'block',
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    [theme.breakpoints.down('sm')]: {
-      padding: theme.spacing(2),
-    },
-  },
 }))
 
 function App() {
@@ -40,15 +27,12 @@ function App() {
   return (
     <Div100vh>
       <div className={styles.container}>
-        <Header />
-        <div className={styles.body}>
-          <BrowserRouter>
-            <Switch>
-              <Route path={'/gallery/:id'} exact component={Gallery} />
-              <Route path={'*'} exact component={Pages} />
-            </Switch>
-          </BrowserRouter>
-        </div>
+        <BrowserRouter>
+          <Switch>
+            <Route path={'/gallery/:id'} exact component={Gallery} />
+            <Route path={'*'} exact component={Pages} />
+          </Switch>
+        </BrowserRouter>
       </div>
       <Error />
     </Div100vh>
